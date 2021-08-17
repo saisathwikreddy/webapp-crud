e9b6826d33686d74bd5dbd0048d46b440454deeb
Yuvasree Raghavan <yuva17336.cs@rmkec.ac.in>
2021-08-17 10:20:11 +0530

    backend

--- app.js
+++ app.js
@@ -1,1 +1,130 @@
-console.log("my webapp");
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+const cors=require('cors');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.get('/',(req,res)=>{
+   try{
+    var jsonData = fs.readFileSync(__dirname+'/data.json')
+
+    // console.log(jsonData)
+    jsonData = JSON.parse(jsonData)
+    jsonData = jsonData["data"]
+   
+    //jsonData = fs.readFileSync(__dirname+'/data.json')
+  
+    res.status(200).send(jsonData);
+   }
+   finally{
+       console.log("Success!")
+   }
+ 
+
+})
+
+app.post('/',(req,res)=>{
+    try{
+    var jsonData = fs.readFileSync(__dirname+'/data.json')
+
+    // console.log(req.body)
+    jsonData = JSON.parse(jsonData)
+    var temp = {}
+    jsonData = jsonData["data"]
+
+    jsonData.push(req.body)
+    temp["data"]=jsonData
+    
+    fs.writeFileSync(__dirname+'/data.json', JSON.stringify(temp))
+
+    var jsonData = fs.readFileSync(__dirname+'/data.json')
+    res.status(201).send(jsonData);}
+    finally{
+        console.log("Data added")
+    }
+
+})
+
+app.put('/:id',(req,res)=>{
+try{
+    var isUpdated = false   
+
+    var jsonData = fs.readFileSync(__dirname+'/data.json')
+
+    // console.log(req.body)
+    jsonData = JSON.parse(jsonData)
+    jsonData = jsonData["data"]
+    
+    var newJson = []
+    jsonData.forEach(item => {
+        if(item["id"] != req.params.id)
+            newJson.push(item)
+        else
+            {
+                newJson.push(req.body)
+                isUpdated = true
+            }
+    });
+   //console.log(newJson)
+    if(isUpdated){
+        var temp = {}
+        temp["data"]=newJson
+    
+        fs.writeFileSync(__dirname+'/data.json', JSON.stringify(temp))
+        // console.log(req.params)
+
+        var jsonData = fs.readFileSync(__dirname+'/data.json')
+        res.status(200).send(jsonData);
+    }}
+    catch{
+        res.status(404).send("No such data!")
+    }
+    
+})
+
+app.delete('/:id',(req,res)=>{
+   try{
+    var isDeleted = false
+
+    var jsonData = fs.readFileSync(__dirname+'/data.json')
+
+    //console.log(req.body)
+    jsonData = JSON.parse(jsonData)
+    jsonData = jsonData["data"]
+    
+    var newJson = []
+    jsonData.forEach(item => {
+    
+        if(item["id"] != parseInt(req.params.id)){
+           console.log(typeof item["id"])
+           console.log(typeof req.params.id)
+            newJson.push(item)}
+        else
+            isDeleted = true
+    });
+ 
+    if(isDeleted){
+        var temp = {}
+        temp["data"]=newJson
+    
+        fs.writeFileSync(__dirname+'/data.json', JSON.stringify(temp))
+        console.log(req.params)
+        
+        var jsonData = fs.readFileSync(__dirname+'/data.json')
+        res.status(200).send(jsonData);
+        
+    }}
+    catch{
+        res.status(404).send("No such data!")
+    }
+    
+})
+
+app.listen(port,()=>{
+    console.log("Server is running:"+port)
+})
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+const port = 5000
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+var arrayValue =  []
+
+app.get('/',(req,res)=>{
+    res.status(200).send(arrayValue)
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    arrayValue.push(1)
+    res.status(200).send(arrayValue)
+})
+
+app.listen(port, ()=>{
+    console.log("Server is running is: "+port)
+})
